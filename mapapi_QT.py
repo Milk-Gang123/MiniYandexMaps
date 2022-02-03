e9df4df493c19e5f37ff6219d51efd5fa6ba105a
BearForesterSmallBeef <BearForest2006@gmail.com>
2022-02-03 19:29:39 +0300

    made switching modes

--- mapapi_QT.py
+++ mapapi_QT.py
@@ -1,36 +1,70 @@
 import sys
-import random
-import requests
 
-from PIL import Image
+from PyQt5 import QtCore, QtWidgets
 from PyQt5.QtGui import QPixmap
-from PyQt5 import QtCore, QtGui, QtWidgets
-from PyQt5.QtWidgets import QApplication, QLabel, QMainWindow, QFileDialog
-
-from io import BytesIO
+from PyQt5.QtWidgets import QApplication, QMainWindow
 
 from geocoder import *
 
 SCREEN_SIZE = [600, 450]
-lat, lon = "37.619727", "55.750536"
+LAT, LON = "37.620447", "55.751034"
+MAX_SCALE, MIN_SCALE = 19, 1
 
 
 class MapWidget(QtWidgets.QWidget):
+    pointer_style = 'pm2' + 'org' + 'l'
+    l_types = ["map", "sat", "skl"]
+
     def __init__(self, parent, lat, lon):
         super().__init__(parent)
-        self.map = self.get_image(lat, lon)
-        self.pixmap = QPixmap()
-        self.pixmap.loadFromData(self.map)
+        self.l_pos = 0
+        self.params = {'lat': lat, 'lon': lon, 'z': 18, 'pt': '', "l": self.l_types[self.l_pos]}
         self.label = QtWidgets.QLabel(self)
-        self.label.resize(*SCREEN_SIZE)
-        self.label.setPixmap(self.pixmap)
+        self.map = None
+        self.pixmap = QPixmap()
+        self.update_image(**self.params)
 
-    def get_image(self, lan, lon):
-        image = get_static_map(lan, lon)
+
+    def get_image(self, lat, lon, **params):
+        image = get_static_map(lat, lon, **params)
         return image
 
+    def update_image(self, **params):
+        self.map = self.get_image(**params)
+        self.set_image(self.map)
+
     def set_image(self, image):
+        self.pixmap.loadFromData(image)
         self.label.setPixmap(self.pixmap)
+
+    def move_map(self, key):
+        delta = 298.033 * math.e ** (-0.678 * self.params['z'])
+        if key == 16777236:
+            self.params['lat'] = str(float(self.params['lat']) + delta)
+        elif key == 16777234:
+            self.params['lat'] = str(float(self.params['lat']) - delta)
+        elif key == 16777235:
+            self.params['lon'] = str(float(self.params['lon']) + delta)
+        elif key == 16777237:
+            self.params['lon'] = str(float(self.params['lon']) - delta)
+        elif key == 1050:
+            self.l_pos = (self.l_pos + 1) % len(self.l_types)
+        self.params["l"] = self.l_types[self.l_pos]
+        self.params['lat'] = str(max(min(float(self.params['lat']), 180), -180))
+        self.params['lon'] = str(max(min(float(self.params['lon']), 90), -90))
+        self.update_image(**self.params)
+
+    def scale_map(self, key):
+        if key == QtCore.Qt.Key.Key_PageUp:
+            self.params['z'] = self.params['z'] + 1
+        elif key == QtCore.Qt.Key.Key_PageDown:
+            self.params['z'] = self.params['z'] - 1
+        self.params['z'] = max(min(self.params['z'], MAX_SCALE), MIN_SCALE)
+        self.update_image(**self.params)
+
+    def add_pointer(self, lat, lon):
+        pointer = f'{lat},{lon},{self.pointer_style}'
+        self.params['pt'] += pointer
 
 
 class Ui_MainWindow(object):
@@ -41,7 +75,7 @@ class Ui_MainWindow(object):
         self.centralwidget.setObjectName("centralwidget")
         self.gridLayout = QtWidgets.QGridLayout(self.centralwidget)
         self.gridLayout.setObjectName("gridLayout")
-        self.widget = MapWidget(self.centralwidget, lat, lon)
+        self.widget = MapWidget(self.centralwidget, LAT, LON)
         self.widget.setObjectName("widget")
         self.gridLayout.addWidget(self.widget, 0, 0, 1, 1)
         MainWindow.setCentralWidget(self.centralwidget)
@@ -51,6 +85,12 @@ class Ui_MainWindow(object):
         MainWindow.setMenuBar(self.menubar)
         self.statusbar = QtWidgets.QStatusBar(MainWindow)
         self.statusbar.setObjectName("statusbar")
+        self.lineEdit = QtWidgets.QLineEdit(MainWindow)
+        self.lineEdit.setText('Россия, Москва, Большой Кремлёвский сквер')
+        self.lineEdit.setGeometry(10, 420, 480, 25)
+        self.button = QtWidgets.QPushButton(MainWindow)
+        self.button.setGeometry(490, 420, 100, 25)
+        self.button.setText('Искать')
         MainWindow.setStatusBar(self.statusbar)
 
         self.retranslateUi(MainWindow)
@@ -64,8 +104,10 @@ class Ui_MainWindow(object):
 class MainWindow(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
+        self.setFocusPolicy(QtCore.Qt.ClickFocus)
         self.setupUi(self)
         self.initUI()
+        self.button.clicked.connect(self.search)
 
     def initUI(self):
         self.setGeometry(400, 400, *SCREEN_SIZE)
@@ -73,6 +115,23 @@ class MainWindow(QMainWindow, Ui_MainWindow):
 
     def load_image(self, image):
         pass
+
+    def keyPressEvent(self, event):
+        self.widget.move_map(event.key())
+        self.widget.scale_map(event.key())
+
+    def search(self):
+        try:
+            address = self.lineEdit.text()
+            toponym = get_toponym(geocode(address))
+            cords = [str(i) for i in get_coordinates(toponym)]
+            self.widget.params['lat'] = cords[0]
+            self.widget.params['lon'] = cords[1]
+            self.widget.add_pointer(*cords)
+            self.widget.update_image(**self.widget.params)
+        except Exception as e:
+            print('Неверный запрос')
+        self.widget.setFocus()
 
 
 if __name__ == '__main__':
